f8a41df09b62a86caaf9d05b2930b20b1518a69b
Leo Lu <leolu@microsoft.com>
2021-05-22 18:22:07 +0800

    uft

--- azureiotrocksB2C/spaPortal/puttogether.js
+++ azureiotrocksB2C/spaPortal/puttogether.js
@@ -5,12 +5,15 @@ const b2cPolicies = {
     authorityDomain: "azureiotb2c.b2clogin.com"
 }
 
+console.log(window.location.href)
+
 const msalConfig = {
     auth: {
         clientId: "f4693be5-601b-4d0e-9208-c35d9ad62387", // This is the ONLY mandatory field; everything else is optional.
         authority: b2cPolicies.signUpSignInAuthority, // Choose sign-up/sign-in user-flow as your default.
         knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
-        redirectUri: "https://azureiotrocksspa.z23.web.core.windows.net"
+        //redirectUri: "https://azureiotrocksspa.z23.web.core.windows.net"
+        redirectUri: "http://localhost:5500/azureiotrocksB2C/spaPortal/spaindex.html"
     },
     cache: {
         cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
