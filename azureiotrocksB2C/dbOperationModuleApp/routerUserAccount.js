d8791302045f8af0179434b65b10c14153e7d971
Leo Lu <leolu@microsoft.com>
2021-06-11 12:43:56 +0800

    dev

--- azureiotrocksB2C/dbOperationModuleApp/routerUserAccount.js
+++ azureiotrocksB2C/dbOperationModuleApp/routerUserAccount.js
@@ -4,12 +4,29 @@ const cosmosdbhelper = require('./cosmosdbhelper')
 function routerUserAccount(){
     this.router = express.Router();
     this.useRoute("basic","post")
+    this.useRoute("checkTwinName","post")
 }
 
 routerUserAccount.prototype.useRoute=function(routeStr,isPost){
     this.router[(isPost)?"post":"get"]("/"+routeStr,(req,res)=>{
         this[routeStr](req,res)
     })
+}
+
+routerUserAccount.prototype.checkTwinName =async function(req,res) {
+    var accountID=req.body.account
+    var checkName= req.body.checkName
+    var queryStr='SELECT * FROM c where '
+    queryStr+=`c.accountID='${accountID}'`
+    queryStr+=` and c.type='DTTwin'`
+    queryStr+=` and c.displayName='${checkName}'`
+    try{
+        var queryResult=await cosmosdbhelper.query('appuser',queryStr)
+        if(queryResult.length==0) res.send(true)
+        else res.send(false)
+    }catch(e){
+        res.status(400).send(e.message)
+    }
 }
 
 routerUserAccount.prototype.basic =async function(req,res) {
