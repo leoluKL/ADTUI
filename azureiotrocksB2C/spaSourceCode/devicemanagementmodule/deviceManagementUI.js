0812f0d73313e903ada28059292eb9f37d1d6a41
Leo Lu <leolu@microsoft.com>
2021-06-23 16:02:44 +0800

    dev

--- azureiotrocksB2C/spaSourceCode/devicemanagementmodule/deviceManagementUI.js
+++ azureiotrocksB2C/spaSourceCode/devicemanagementmodule/deviceManagementUI.js
@@ -2,13 +2,18 @@
 const globalAppSettings = require("../globalAppSettings.js");
 const msalHelper=require("../msalHelper")
 const deviceManagementMainToolbar = require("./deviceManagementMainToolbar")
+const modelEditorDialog = require("../sharedSourceFiles/modelEditorDialog")
+const modelManagerDialog = require("../sharedSourceFiles/modelManagerDialog")
 const globalCache=require("../sharedSourceFiles/globalCache")
+const twinsList=require("./twinsList")
 
 function deviceManagementUI() {
+    this.twinsList= new twinsList($("#TwinsList"))
     deviceManagementMainToolbar.render()
 
     this.myMSALObj = new msal.PublicClientApplication(globalAppSettings.msalConfig);
 
+    this.broadcastMessage()
 
     var theAccount=msalHelper.fetchAccount();
     if(theAccount==null && !globalAppSettings.isLocalTest) window.open(globalAppSettings.logoutRedirectUri,"_self")
@@ -16,6 +21,25 @@ function deviceManagementUI() {
     globalCache.loadUserData().then(re=>{
         //TODO: if there is no model at all, prompt user to create his first model
     })
+}
+
+deviceManagementUI.prototype.broadcastMessage=function(source,msgPayload){
+    var componentsArr=[modelManagerDialog,modelEditorDialog,deviceManagementMainToolbar]
+
+    if(source==null){
+        for(var i=0;i<componentsArr.length;i++){
+            var theComponent=componentsArr[i]
+            this.assignBroadcastMessage(theComponent)
+        }
+    }else{
+        for(var i=0;i<componentsArr.length;i++){
+            var theComponent=componentsArr[i]
+            if(theComponent.rxMessage && theComponent!=source) theComponent.rxMessage(msgPayload)
+        }
+    }
+}
+deviceManagementUI.prototype.assignBroadcastMessage=function(uiComponent){
+    uiComponent.broadcastMessage=(msgObj)=>{this.broadcastMessage(uiComponent,msgObj)}
 }
 
 
---
+++ azureiotrocksB2C/spaSourceCode/devicemanagementmodule/twinsList.js
@@ -0,0 +1,9 @@
+const msalHelper = require("../msalHelper")
+const globalCache = require("../sharedSourceFiles/globalCache")
+
+
+function twinsList(DOM) {
+    
+}
+
+module.exports = twinsList;
