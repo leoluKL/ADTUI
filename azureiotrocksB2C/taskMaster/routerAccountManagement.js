58b655ea9784a905b8bc877ac9675ed451d59f41
Leo Lu <leolu@microsoft.com>
2021-07-10 14:12:25 +0800

    dev

--- azureiotrocksB2C/taskMaster/routerAccountManagement.js
+++ azureiotrocksB2C/taskMaster/routerAccountManagement.js
@@ -4,12 +4,65 @@ const got = require('got');
 function routerAccountManagement(){
     this.router = express.Router();
     this.useRoute("fetchUserAccount")
+    this.useRoute("changeOwnProjectName","post")
+    this.useRoute("shareProjectTo","post")
+    this.useRoute("newProjectTo","post")
+    this.useRoute("deleteProjectTo","post")
+    this.useRoute("notShareProjectTo","post")
 }
 
 routerAccountManagement.prototype.useRoute=function(routeStr,isPost){
     this.router[(isPost)?"post":"get"]("/"+routeStr,(req,res)=>{
         this[routeStr](req,res)
     })
+}
+
+routerAccountManagement.prototype.changeOwnProjectName =async function(req,res) {
+    var reqBody=req.body
+    reqBody.requestFromAccount=req.authInfo.account
+    try{
+        var {body} = await got.post(process.env.dboperationAPIURL+"userAccount/changeOwnProjectName", {json:reqBody,responseType: 'json'});
+        res.send(body)
+    }catch(e){
+        res.status(e.response.statusCode).send(e.response.body);
+    }  
+}
+
+routerAccountManagement.prototype.shareProjectTo =async function(req,res) {
+    var reqBody=req.body
+    reqBody.requestFromAccount=req.authInfo.account
+    try{
+        var {body} = await got.post(process.env.dboperationAPIURL+"userAccount/shareProjectTo", {json:reqBody,responseType: 'json'});
+        res.send(body)
+    }catch(e){
+        res.status(e.response.statusCode).send(e.response.body);
+    }  
+}
+routerAccountManagement.prototype.newProjectTo =async function(req,res) {
+    var reqBody=req.body
+    reqBody.account=req.authInfo.account
+    try{
+        var {body} = await got.post(process.env.dboperationAPIURL+"userAccount/newProjectTo", {json:reqBody,responseType: 'json'});
+        res.send(body)
+    }catch(e){
+        res.status(e.response.statusCode).send(e.response.body);
+    }  
+}
+
+routerAccountManagement.prototype.notShareProjectTo =async function(req,res) {
+    var reqBody=req.body
+    reqBody.requestFromAccount=req.authInfo.account
+    try{
+        var {body} = await got.post(process.env.dboperationAPIURL+"userAccount/notShareProjectTo", {json:reqBody,responseType: 'json'});
+        res.send(body)
+    }catch(e){
+        res.status(e.response.statusCode).send(e.response.body);
+    }  
+}
+
+routerAccountManagement.prototype.deleteProjectTo =async function(req,res) {
+    //TODO:
+    res.status(400).send("Deleting project is not available yet");
 }
 
 routerAccountManagement.prototype.fetchUserAccount =async function(req,res) {
