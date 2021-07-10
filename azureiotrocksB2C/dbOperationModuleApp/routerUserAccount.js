2ff65138814b4405437cb1b066d0b93442c69b57
Leo Lu <leolu@microsoft.com>
2021-07-10 14:01:52 +0800

    dev

--- azureiotrocksB2C/dbOperationModuleApp/routerUserAccount.js
+++ azureiotrocksB2C/dbOperationModuleApp/routerUserAccount.js
@@ -6,6 +6,10 @@ function routerUserAccount(){
     this.useRoute("basic","post")
     this.useRoute("checkTwinName","post")
     this.useRoute("changeOwnProjectName","post")
+    this.useRoute("shareProjectTo","post")
+    this.useRoute("newProjectTo","post")
+    this.useRoute("deleteProjectTo","post")
+    this.useRoute("notShareProjectTo","post")
 }
 
 routerUserAccount.prototype.useRoute=function(routeStr,isPost){
@@ -30,6 +34,13 @@ routerUserAccount.prototype.checkTwinName =async function(req,res) {
     }
 }
 
+routerUserAccount.prototype.uuidv4=function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
 routerUserAccount.prototype.basic =async function(req,res) {
     var accountID=req.body.account
     var idp= req.body.idp
@@ -45,13 +56,8 @@ routerUserAccount.prototype.basic =async function(req,res) {
     
 
     if(queryResult.length==0){
-        function uuidv4() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
-            });
-        }
-        var firstProjectID=uuidv4()
+        
+        var firstProjectID=this.uuidv4()
         //create the new account record in database and return it
         queryResult.push({
             "id": accountID,
@@ -71,11 +77,139 @@ routerUserAccount.prototype.basic =async function(req,res) {
     res.send(queryResult[0])
 }
 
-routerUserAccount.prototype.changeOwnProjectName =async function(req,res) {
-    var accountIDs=req.body.accounts
-    var projectID= req.body.projectID
-    var newName=req.body.newProjectName
+routerUserAccount.prototype.newProjectTo =async function(req,res) {
+    var ownerAccount=req.body.account
+    var projectID= this.uuidv4()
+    var projectName=req.body.projectName
+    try{
+        var accountDocument=await this.getUserAccountDocument(ownerAccount)
+        accountDocument.joinedProjects.push( {"id":projectID, "name": projectName, "owner": ownerAccount })
+        await cosmosdbhelper.insertRecord("appuser",accountDocument)
+        res.end()
+    }catch(e){
+        res.status(400).send(e.message);
+        return;
+    }
+}
 
+routerUserAccount.prototype.deleteProjectTo =async function(req,res) {
+    //TODO: delete all related project data
+}
+
+routerUserAccount.prototype.notShareProjectTo = async function (req, res) {
+    var requestFromAccount = req.body.requestFromAccount
+    var notshareToAccount = req.body.notShareToAccount
+    if(requestFromAccount==this.notShareProjectTo){
+        res.status(400).send("Can not remove accessing to own project")
+        return;
+    }
+    var projectID = req.body.projectID
+    try {
+        var projectName = await this.getProjectNameIfAccountOwnProject(requestFromAccount, projectID)
+        if (projectName == null) {
+            res.status(400).send(requestFromAccount + " is not authorized to change this project")
+            return;
+        }
+
+        try{
+            var accountDocument = await this.getUserAccountDocument(notshareToAccount)
+        }catch(e){ //in case the target account does not exist
+            res.end()
+            return
+        }
+        
+        var joinedProjects = accountDocument.joinedProjects
+        for (var i = 0; i < joinedProjects.length; i++) {
+            var oneProject = joinedProjects[i]
+            if (oneProject.id == projectID) {
+                joinedProjects.splice(i,1)
+                break
+            }
+        }
+        await cosmosdbhelper.insertRecord("appuser", accountDocument)
+        res.end()
+    } catch (e) {
+        res.status(400).send(e.message);
+        return;
+    }
+}
+
+routerUserAccount.prototype.shareProjectTo =async function(req,res) {
+    var requestFromAccount=req.body.requestFromAccount
+    var shareToAccount=req.body.shareToAccount
+    var projectID= req.body.projectID
+    try{
+        var projectName=await this.getProjectNameIfAccountOwnProject(requestFromAccount,projectID)
+        if(projectName==null){
+            res.status(400).send(requestFromAccount+" is not authorized to change this project")
+            return;
+        }
+
+        var accountDocument=await this.getUserAccountDocument(shareToAccount)
+        var joinedProjects=accountDocument.joinedProjects
+        for(var i=0;i<joinedProjects.length;i++){
+            var oneProject=joinedProjects[i]
+            if(oneProject.id==projectID){
+                res.end() //it is already shared to the target account
+                return;
+            }
+        }
+        accountDocument.joinedProjects.push( {"id":projectID, "name": projectName, "owner": requestFromAccount })
+        await cosmosdbhelper.insertRecord("appuser",accountDocument)
+        res.end()
+    }catch(e){
+        res.status(400).send(e.message);
+        return;
+    }
+}
+
+
+routerUserAccount.prototype.getUserAccountDocument =async function(accountID) {
+    var queryStr='SELECT * FROM c where '
+    queryStr+=`c.accountID='${accountID}'`
+    try{
+        var queryResult=await cosmosdbhelper.query('appuser',queryStr)
+        if(queryResult.length==0) throw new Error("account "+accountID+" does not exist"); 
+        var wholeDocument=queryResult[0]
+        return wholeDocument
+    }catch(e){
+        throw e
+    }
+}
+
+routerUserAccount.prototype.getProjectNameIfAccountOwnProject =async function(accountID,projectID) {
+    var projectName=null
+    try{
+        var accountDocument=await this.getUserAccountDocument(accountID)
+        var joinedProjects=accountDocument.joinedProjects
+        for(var i=0;i<joinedProjects.length;i++){
+            var oneProject=joinedProjects[i]
+            if(oneProject.id==projectID){
+                if(oneProject.owner==accountID) projectName=oneProject.name
+                break
+            }
+        }
+    }catch(e){
+        throw e
+    }
+    return projectName;
+}
+
+routerUserAccount.prototype.changeOwnProjectName =async function(req,res) {
+    var requestFromAccount=req.body.requestFromAccount
+    var projectID= req.body.projectID
+    var accountIDs=req.body.accounts
+    var newName=req.body.newProjectName
+    try{
+        var projectName=await this.getProjectNameIfAccountOwnProject(requestFromAccount,projectID)
+        if(projectName==null){
+            res.status(400).send(requestFromAccount+" is not authorized to change this project")
+            return;
+        }
+    }catch(e){
+        res.status(400).send(e.message);
+        return;
+    }
     var promiseArr=[]
     accountIDs.forEach(oneAccount=>{
         oneAccount=oneAccount.toLowerCase();
@@ -90,11 +224,8 @@ routerUserAccount.prototype.changeOwnProjectName =async function(req,res) {
 }
 
 routerUserAccount.prototype.changeProjectNameInOneAccount =async function(accountID,projectID,newName) {
-    var queryStr='SELECT * FROM c where '
-    queryStr+=`c.accountID='${accountID}'`
     try{
-        var queryResult=await cosmosdbhelper.query('appuser',queryStr)
-        var wholeDocument=queryResult[0]
+        var wholeDocument=await this.getUserAccountDocument(accountID)
         var joinedProjects=wholeDocument.joinedProjects
         for(var i=0;i<joinedProjects.length;i++){
             var oneProject=joinedProjects[i]
