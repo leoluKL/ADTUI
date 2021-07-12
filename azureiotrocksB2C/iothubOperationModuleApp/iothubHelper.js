424889860d43bcd6f0704a3a8ac68273fab22e9f
Leo Lu <leolu@microsoft.com>
2021-07-12 18:03:04 +0800

    dev

--- azureiotrocksB2C/iothubOperationModuleApp/iothubHelper.js
+++ azureiotrocksB2C/iothubOperationModuleApp/iothubHelper.js
@@ -6,7 +6,19 @@ function iothubHelper() {
     const credential = new DefaultAzureCredential();
     this.iothubClient = new iothub.Client.fromTokenCredential(process.env.IoTHubEndpoint,credential)
     this.iothubRegistry = new iothub.Registry.fromTokenCredential(process.env.IoTHubEndpoint,credential)
+
+    this.manualRenewToken()
 }
 
+
+iothubHelper.prototype.manualRenewToken=function(){
+    //it might be a bug that token is not renewed now, bug report: https://github.com/Azure/azure-iot-sdk-node/issues
+    //manually refresh token every hour
+    setInterval(() => {
+        this.iothubClient = new iothub.Client.fromTokenCredential(process.env.IoTHubEndpoint,credential)
+        this.iothubRegistry = new iothub.Registry.fromTokenCredential(process.env.IoTHubEndpoint,credential)
+    }, 3600*1000);
+
+}
 
 module.exports = new iothubHelper();
